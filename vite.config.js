2b284eb34773db2024e8f16d86d3322ed3f2fcd9
milkysx <milkysx>
2025-05-04 20:52:15 +0300

    add vite.config

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        feedback: resolve(__dirname, 'feedback.html'),
+        reviews: resolve(__dirname, 'reviews.html')
+      }
+    }
+  }
+})
