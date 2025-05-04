6bc3062d942b7f5eede775365ca970534ebb78c4
milkysx <milkysx>
2025-05-04 14:31:29 +0300

    create a feedback page and fix a home page

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,26 @@
 import '../scss/style.scss';
+
+
+$('.reviews__slider_one').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    adaptiveHeight: true,
+    pauseOnHover: true,
+});
+
+$('.reviews__slider_two').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    adaptiveHeight: true,
+    pauseOnHover: true,
+});
